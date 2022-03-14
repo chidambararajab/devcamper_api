be85a8cc7e3a9d8280b14812157d1e7e1846ebe7
chidambararaja.b <chidambararaja.b@accubits.com>
2022-03-15 02:50:38 +0530

    [get/]

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Api is running...");
+});
+
+console.log("process.env :>> ", process.env);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}.`)
+  console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`)
 );
